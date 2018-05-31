93d77b73a804a1ef32075f5aa4dc2ebcf1964dc4
felipeaugustox <augustto.felipe@hotmail.com>
2018-05-31 19:00:38 -0300

    Fix user create

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -20,6 +20,7 @@ const fetchUser = () => {
 */
 export const fetchUser = () => async (dispatch) => {
     const res = await axios.get('/api/current_user')
+
     dispatch({ type: FETCH_USER, payload: res.data })
 }
 
